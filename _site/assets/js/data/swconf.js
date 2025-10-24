243b6ec9a54adc9c0683e651f5627a6ab766aa3f
mohan kumar bn <mkbntech@gmail.com>
2025-10-24 21:36:27 +0530

    Update path

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1761320575',resources: [
+    cacheName: 'chirpy-1761321883',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
