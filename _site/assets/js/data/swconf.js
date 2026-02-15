6ce33518dbfdb09a9cdea4ceee6acbe27cf27fb8
mbn02 <mohankumarbn02@gmail.com>
2026-02-15 23:25:27 +0530

    Added new post: ai powered kuberntes troubleshooting

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1771160728',resources: [
+    cacheName: 'chirpy-1771178059',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
