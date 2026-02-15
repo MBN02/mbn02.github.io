9030627de73045113ae41f0e16679317f46b6589
mbn02 <mohankumarbn02@gmail.com>
2026-02-15 07:15:37 +0530

    Added new post: Resolve image issue

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1762578532',resources: [
+    cacheName: 'chirpy-1771119839',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
